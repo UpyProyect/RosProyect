f20bbd70a2710b64f5dc745a924ff2b4b7e58dde
Edwin Miguel Hoil Majay <53419453+edwin221b@users.noreply.github.com>
2019-08-05 00:25:01 -0500

    Add files via upload

---
+++ Examples/battery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import psutil
+battery = psutil.sensors_battery()
+plugged = battery.power_plugged
+percent = str(battery.percent)
+if plugged==False: plugged="Not Plugged In"
+else: plugged="Plugged In"
+print(percent+'% | '+plugged)
+
